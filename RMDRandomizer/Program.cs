ccb9c3003d8225f31f12cf6eda03a13ca6182114
Ryan ShrineFox <ShrineFox@users.noreply.github.com>
2017-06-06 02:17:11 -0400

    Added P3 support, RMD mixing between games and ADX randomization

--- RMDRandomizer/Program.cs
+++ RMDRandomizer/Program.cs
@@ -11,27 +11,37 @@ namespace RMDRandomizer
     {
         static void Main(string[] args)
         {
-            List<string> RMDFiles = new List<string>();
-            List<string> PACFiles = new List<string>();
-            Console.Write("Path to Persona 4 data/MODEL directory: ");
-            string modelPath = Console.ReadLine();
-            foreach (string file in Directory.GetFiles(modelPath, "*.RMD*", System.IO.SearchOption.AllDirectories))
+            bool validSelection = false;
+            while (validSelection == false)
             {
-                RMDFiles.Add(file);
+                Console.WriteLine("Select a mode:\np4 - Persona 4 RMD shuffle\np3 - Persona 3 FES RMD Shuffle");
+                string selection = Console.ReadLine().ToUpper();
+                if (selection == "P4") { P4Randomized(); validSelection = true; }
+                if (selection == "P3") { P3Randomized(); validSelection = true; }
+                else { Console.Clear(); }
             }
-            foreach (string file in Directory.GetFiles(modelPath, "*.PAC*", System.IO.SearchOption.AllDirectories))
+
+            Console.WriteLine("Everything has been randomized successfully!");
+            Console.ReadKey();
+        }
+
+        static void P4Randomized()
+        {
+            List<string> RMDFiles = Randomizer.GetP4RMDs();
+            Console.Write("Output directory: ");
+            string outputDir = Console.ReadLine();
+
+            bool validSelection = false;
+            while (validSelection == false)
             {
-                PACFiles.Add(file);
-            }
-            Console.Write("Path to Persona 4 btl/MODEL_BTL directory: ");
-            modelPath = Console.ReadLine();
-            foreach (string file in Directory.GetFiles(modelPath, "*.RMD*", System.IO.SearchOption.AllDirectories))
-            {
-                RMDFiles.Add(file);
-            }
-            foreach (string file in Directory.GetFiles(modelPath, "*.PAC*", System.IO.SearchOption.AllDirectories))
-            {
-                PACFiles.Add(file);
+                Console.Write("Would you like to include RMDs from Persona 3? (y/n): ");
+                string selection = Console.ReadLine().ToUpper();
+                if (selection == "Y") {
+                    validSelection = true;
+                    RMDFiles.AddRange(Randomizer.GetP3RMDs());
+                }
+                if (selection == "N") { validSelection = true; }
+                else { Console.Clear(); }
             }
 
             Console.WriteLine("---------------------------------");
@@ -39,79 +49,236 @@ namespace RMDRandomizer
 
             Random rng = new Random();
             string[] RandomizedRMDFiles = RMDFiles.ToArray().OrderBy(x => rng.Next()).ToArray();
-            string[] RandomizedPACFiles = PACFiles.ToArray().OrderBy(x => rng.Next()).ToArray();
 
-            Directory.CreateDirectory("MODEL");
-            Directory.CreateDirectory("MODEL\\FACILITYP");
-            Directory.CreateDirectory("MODEL\\FIELD");
-            Directory.CreateDirectory("MODEL\\NPC");
-            Directory.CreateDirectory("MODEL\\NPC2");
-            Directory.CreateDirectory("MODEL\\PACK");
-            Directory.CreateDirectory("MODEL\\SYMBOL");
-            Directory.CreateDirectory("MODEL\\WEAPON");
-            Directory.CreateDirectory("MODEL_BTL\\PERSONA");
-            Directory.CreateDirectory("MODEL_BTL\\PACK");
+            List<string> dirListP4 = Randomizer.P4Dirs(outputDir);
+            
+            Randomizer.CreateDirs(dirListP4, outputDir);
 
             //FACILITYP
             for (int i = 0; i < 256; i++)
             {
                 string newFileName = RMDFiles[i].Split('\\').Last();
-                File.Copy(RandomizedRMDFiles[i], $"MODEL\\FACILITYP\\{newFileName}");
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[4]}\\{newFileName}");
             }
-
+            Console.WriteLine("FACILITYP randomized");
             //FIELD
             for (int i = 256; i < 283; i++)
             {
                 string newFileName = RMDFiles[i].Split('\\').Last();
-                File.Copy(RandomizedRMDFiles[i], $"MODEL\\FIELD\\{newFileName}");
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[5]}\\{newFileName}");
             }
-
+            Console.WriteLine("FIELD randomized");
             //NPC
             for (int i = 283; i < 1183; i++)
             {
                 string newFileName = RMDFiles[i].Split('\\').Last();
-                File.Copy(RandomizedRMDFiles[i], $"MODEL\\NPC\\{newFileName}");
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[6]}\\{newFileName}");
             }
-
+            Console.WriteLine("NPC randomized");
             //NPC2
             for (int i = 1183; i < 1820; i++)
             {
                 string newFileName = RMDFiles[i].Split('\\').Last();
-                File.Copy(RandomizedRMDFiles[i], $"MODEL\\NPC2\\{newFileName}");
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[7]}\\{newFileName}");
             }
-
+            Console.WriteLine("NPC2 randomized");
             //SYMBOL
             for (int i = 1820; i < 1825; i++)
             {
                 string newFileName = RMDFiles[i].Split('\\').Last();
-                File.Copy(RandomizedRMDFiles[i], $"MODEL\\SYMBOL\\{newFileName}");
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[8]}\\{newFileName}");
             }
-
+            Console.WriteLine("SYMBOL randomized");
             //WEAPON
             for (int i = 1826; i < 2157; i++)
             {
                 string newFileName = RMDFiles[i].Split('\\').Last();
-                File.Copy(RandomizedRMDFiles[i], $"MODEL\\WEAPON\\{newFileName}");
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[9]}\\{newFileName}");
             }
-
-            //PERSONA
-            for (int i = 2157; i < 2413; i++)
+            Console.WriteLine("WEAPON randomized");
+            //FIELD RMD
+            for (int i = 2157; i < 2532; i++)
             {
                 string newFileName = RMDFiles[i].Split('\\').Last();
-                File.Copy(RandomizedRMDFiles[i], $"MODEL_BTL\\PERSONA\\{newFileName}");
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[3]}\\{newFileName}");
             }
-
-            Console.WriteLine("RMDs randomized successfully!");
-            Console.WriteLine("PAC Count: " + PACFiles.Count());
-
-            //ENEMY
-            for (int i = 91; i < 427; i++)
+            Console.WriteLine("FIELD/RMD randomized");
+            //PERSONA
+            for (int i = 2532; i < 2788; i++)
             {
-                string newFileName = PACFiles[i].Split('\\').Last();
-                File.Copy(RandomizedPACFiles[i], $"MODEL_BTL\\PACK\\{newFileName}");
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP4[10]}\\{newFileName}");
             }
-            Console.WriteLine("PACs randomized successfully! Press any key to exit.");
-            Console.ReadKey();
+            Console.WriteLine("PERSONA randomized\nRMDs successfully randomized!\n---------------------------------");
+
+            validSelection = false;
+            while (validSelection == false)
+            {
+                Console.WriteLine("Would you like to randomize BGM ADX files? (y/n): ");
+                string selection = Console.ReadLine().ToUpper();
+                if (selection == "Y")
+                {
+                    validSelection = true;
+                    Console.Write("Path to extracted P4 BGM directory: ");
+                    List<string> adxFiles = Randomizer.GetADX(Console.ReadLine());
+                    outputDir = $"{outputDir}\\ADX";
+                    string[] RandomizedADXFiles = adxFiles.ToArray().OrderBy(x => rng.Next()).ToArray();
+
+                    Console.WriteLine("Would you like to include other ADX files? (y/n): ");
+                    selection = Console.ReadLine().ToUpper();
+                    if (selection == "Y")
+                    {
+                        Console.Write("Path to extra ADX files: ");
+                        adxFiles.AddRange(Randomizer.GetADX(Console.ReadLine()));
+                    }
+
+                    if (Directory.Exists(outputDir))
+                    {
+                        try
+                        {
+                            Directory.Delete(outputDir, true);
+                        }
+                        catch
+                        {
+                            Console.WriteLine($"Close or backup any files in \"{outputDir}\" that are still in use and try again.");
+                        }
+                    }
+                    Directory.CreateDirectory(outputDir);
+
+
+                    for (int i = 0; i < 107; i++)
+                    {
+                        string newFileName = adxFiles[i].Split('\\').Last();
+                        File.Copy(RandomizedADXFiles[i], $"{outputDir}\\{newFileName}");
+                    }
+                    Console.WriteLine("ADX files randomized");
+                }
+                if (selection == "N") { validSelection = true; }
+            }
+        }
+
+        static void P3Randomized()
+        {
+            List<string> RMDFiles = Randomizer.GetP3RMDs();
+            Console.Write("Output directory: ");
+            string outputDir = Console.ReadLine();
+
+            bool validSelection = false;
+            while (validSelection == false)
+            {
+                Console.Write("Would you like to include RMDs from Persona 4? (y/n): ");
+                string selection = Console.ReadLine().ToUpper();
+                if (selection == "Y")
+                {
+                    validSelection = true;
+                    RMDFiles.AddRange(Randomizer.GetP4RMDs());
+                }
+                if (selection == "N") { validSelection = true; }
+                else { Console.Clear(); }
+            }
+
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("RMD Count: " + RMDFiles.Count());
+
+            Random rng = new Random();
+            string[] RandomizedRMDFiles = RMDFiles.ToArray().OrderBy(x => rng.Next()).ToArray();
+
+            List<string> dirListP3 = Randomizer.P3Dirs(outputDir);
+            Randomizer.CreateDirs(dirListP3, outputDir);
+
+            //FACILITYP
+            for (int i = 0; i < 256; i++)
+            {
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP3[3]}\\{newFileName}");
+            }
+            Console.WriteLine("FACILITYP randomized");
+            //FIELD
+            for (int i = 256; i < 277; i++)
+            {
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP3[4]}\\{newFileName}");
+            }
+            Console.WriteLine("FIELD randomized");
+            //NPC
+            for (int i = 277; i < 1348; i++)
+            {
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP3[5]}\\{newFileName}");
+            }
+            Console.WriteLine("NPC randomized");
+            //PERSONA
+            for (int i = 1348; i < 1604; i++)
+            {
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP3[6]}\\{newFileName}");
+            }
+            Console.WriteLine("PERSONA randomized");
+            //SYMBOL
+            for (int i = 1604; i < 1635; i++)
+            {
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP3[7]}\\{newFileName}");
+            }
+            Console.WriteLine("SYMBOL randomized");
+            //WEAPON
+            for (int i = 1635; i < 2404; i++)
+            {
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP3[8]}\\{newFileName}");
+            }
+            Console.WriteLine("WEAPON randomized");
+            //FIELD RMD
+            for (int i = 2404; i < 3217; i++)
+            {
+                string newFileName = RMDFiles[i].Split('\\').Last();
+                File.Copy(RandomizedRMDFiles[i], $"{dirListP3[9]}\\{newFileName}");
+            }
+            Console.WriteLine("FIELD/RMD randomized\nRMDs successfully randomized!\n---------------------------------");
+            validSelection = false;
+            while (validSelection == false)
+            {
+                Console.WriteLine("Would you like to randomize BGM ADX files? (y/n): ");
+                string selection = Console.ReadLine().ToUpper();
+                if (selection == "Y")
+                {
+                    validSelection = true;
+                    Console.Write("Path to extracted BGM directory: ");
+                    List<string> adxFiles = Randomizer.GetADX(Console.ReadLine());
+                    outputDir = $"{outputDir}\\ADX";
+                    string[] RandomizedADXFiles = adxFiles.ToArray().OrderBy(x => rng.Next()).ToArray();
+
+                    Console.WriteLine("Would you like to include other ADX files? (y/n): ");
+                    selection = Console.ReadLine().ToUpper();
+                    if (selection == "Y")
+                    {
+                        Console.Write("Path to extra ADX files: ");
+                        adxFiles.AddRange(Randomizer.GetADX(Console.ReadLine()));
+                    }
+                    
+                    if (Directory.Exists(outputDir))
+                        {
+                        try
+                        {
+                            Directory.Delete(outputDir, true);
+                        }
+                        catch
+                        {
+                            Console.WriteLine($"Close or backup any files in \"{outputDir}\" that are still in use and try again.");
+                        }
+                    }
+                    Directory.CreateDirectory(outputDir);
+
+
+                    for (int i = 0; i < 223; i++)
+                    {
+                        string newFileName = adxFiles[i].Split('\\').Last();
+                        File.Copy(RandomizedADXFiles[i], $"{outputDir}\\{newFileName}");
+                    }
+                    Console.WriteLine("ADX files randomized");
+                }
+                if (selection == "N") { validSelection = true; }
+            }
         }
     }
 }
---
+++ RMDRandomizer/Randomizer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDRandomizer
+{
+    class Randomizer
+    {
+        public static List<string> Add(List<string> rmdList, string modelPath)
+        {
+            foreach (string file in Directory.GetFiles(modelPath, "*.RMD*", System.IO.SearchOption.AllDirectories))
+            {
+                rmdList.Add(file);
+            }
+            return rmdList;
+        }
+
+
+        public static List<string> GetP4RMDs()
+        {
+            List<string> RMDFiles = new List<string>();
+            Console.Write("Path to Persona 4 data/MODEL directory: ");
+            RMDFiles = Randomizer.Add(RMDFiles, Console.ReadLine());
+            Console.Write("Path to Persona 4 data/FIELD/RMD directory: ");
+            RMDFiles = Randomizer.Add(RMDFiles, Console.ReadLine());
+            Console.Write("Path to Persona 4 btl/MODEL_BTL directory: ");
+            RMDFiles = Randomizer.Add(RMDFiles, Console.ReadLine());
+            return RMDFiles;
+        }
+
+        public static List<string> GetP3RMDs()
+        {
+            List<string> RMDFiles = new List<string>();
+            Console.Write("Path to Persona 3 btl/MODEL directory: ");
+            RMDFiles = Randomizer.Add(RMDFiles, Console.ReadLine());
+            Console.Write("Path to Persona 3 data/FIELD/RMD directory: ");
+            RMDFiles = Randomizer.Add(RMDFiles, Console.ReadLine());
+            return RMDFiles;
+        }
+
+        public static List<string> P4Dirs(string outputDir)
+        {
+            List<string> dirList = new List<string>() { $"{outputDir}\\P4 RANDOMIZED", $"{outputDir}\\P4 RANDOMIZED\\MODEL", $"{outputDir}\\P4 RANDOMIZED\\FIELD", $"{outputDir}\\P4 RANDOMIZED\\FIELD\\RMD",
+                                                        $"{outputDir}\\P4 RANDOMIZED\\MODEL\\FACILITYP", $"{outputDir}\\P4 RANDOMIZED\\MODEL\\FIELD", $"{outputDir}\\P4 RANDOMIZED\\MODEL\\NPC",
+                                                        $"{outputDir}\\P4 RANDOMIZED\\MODEL\\NPC2", $"{outputDir}\\P4 RANDOMIZED\\MODEL\\SYMBOL", $"{outputDir}\\P4 RANDOMIZED\\MODEL\\WEAPON",
+                                                        $"{outputDir}\\P4 RANDOMIZED\\MODEL_BTL\\PERSONA" };
+            return dirList;
+        }
+
+        public static List<string> P3Dirs(string outputDir)
+        {
+            List<string> dirList = new List<string>() { $"{outputDir}\\P3 RANDOMIZED", $"{outputDir}\\P3 RANDOMIZED\\MODEL", $"{outputDir}\\P3 RANDOMIZED\\FIELD", $"{outputDir}\\P3 RANDOMIZED\\MODEL\\FACILITYP",
+                                                        $"{outputDir}\\P3 RANDOMIZED\\MODEL\\FIELD", $"{outputDir}\\P3 RANDOMIZED\\MODEL\\NPC", $"{outputDir}\\P3 RANDOMIZED\\MODEL\\PERSONA", $"{outputDir}\\P3 RANDOMIZED\\MODEL\\SYMBOL",
+                                                        $"{outputDir}\\P3 RANDOMIZED\\MODEL\\WEAPON", $"{outputDir}\\P3 RANDOMIZED\\FIELD\\RMD" };
+            return dirList;
+        }
+
+
+
+        public static void CreateDirs(List<string> dirList, string outputDir)
+        {
+            foreach (string dir in dirList)
+            {
+                if (Directory.Exists(dir))
+                {
+                    try
+                    {
+                        Directory.Delete(dir, true);
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"Close or backup any files in \"{outputDir}\" that are still in use and try again.");
+                    }
+                }
+                Directory.CreateDirectory(dir);
+                }
+        }
+
+        public static List<string> GetADX(string bgmDir)
+        {
+            List<string> adxFiles = new List<string>();
+            foreach (string file in Directory.GetFiles(bgmDir, "*.ADX*", System.IO.SearchOption.AllDirectories))
+            {
+                adxFiles.Add(file);
+            }
+            return adxFiles;
+        }
+
+    }
+}
